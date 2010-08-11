380b61efb5e5f60042fc65bbbf1aaac66c7cc85f
bao <bao.nguyen@ozerside.net>
2010-08-11 13:50:00 +0700

    Add README and various stuff

---
+++ README.md
@@ -0,0 +1,9 @@
+# LinkHay Chrome Extension
+
+A Chrome Extension for Linkhay.com allows user to submit and vote link directly on the browser.
+
+# License
+
+Released under GPLv3
+
+Copyright (c) 2010 Bao Nguyen
---
+++ linkhay.js
@@ -0,0 +1,11 @@
+/**
+ * LinkHay Chrome Extension
+ * 
+ * LinkHay API Wrapper library
+ *
+ * @link http://api.linkhay.com
+ * @author Bao Nguyen <contact@nqbao.com>
+ * @license GPLv3
+ **/
+ 
+LinkHay = LinkHay || {};
